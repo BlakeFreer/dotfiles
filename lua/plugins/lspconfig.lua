9c86b2179fd323b931824de92744287d21602b5b
Blake Freer <bkfreer10@gmail.com>
2025-06-14 00:22:10 -0700

    Formatting works for other languages

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -156,6 +156,11 @@ return {
                         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
                     end, "[T]oggle Inlay [H]ints")
                 end
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    callback = function()
+                        vim.lsp.buf.format({ async = true })
+                    end,
+                })
             end,
         })
 
