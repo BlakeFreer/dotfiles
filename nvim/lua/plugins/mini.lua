258a58ff31e229166060e52fa127c4997fb870eb
Blake Freer <bkfreer10@gmail.com>
2025-09-09 11:44:24 -0400

    Replace mini-surround with tpope's

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -12,7 +12,9 @@ return { -- Collection of various small independent plugins/modules
         -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
         -- - sd'   - [S]urround [D]elete [']quotes
         -- - sr)'  - [S]urround [R]eplace [)] [']
-        require("mini.surround").setup()
+        --
+        -- Using tpope/surround instead
+        -- require("mini.surround").setup()
 
         -- Simple and easy statusline.
         --  You could remove this setup call if you don't like it,
